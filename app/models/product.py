156497e425fed5aa802a16fea1264ca21b450da0
David Nash <hisownspace@gmail.com>
2022-02-04 12:44:02 -0500

    make product table

---
+++ app/models/product.py
@@ -0,0 +1,20 @@
+from .db import db
+
+class Product(db.Model):
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+    imageUrl = db.Column(db.string(255))
+    price = db.Column(db.Integer(), nullable=False)
+    quantity = db.Column(db.Integer(), nullable=False)
+    userId = db.Column(db.Integer(),
+        db.ForeignKey('users.id', nullable=False))
+    categoryId = db.Column(db.Integer(),
+        db.ForeignKey('categories.id'), nullable=False)
+    createdAt = db.Column(db.Date(), nullable=False)
+    updatedAt = db.Column(db.Date(), nullable=False)
+
+    category = db.relationship('')
+    user = db.relationship('')
