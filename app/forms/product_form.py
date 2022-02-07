f56e7dd53856528249b4a9ac5f08132b41914ad4
Sornam Vairavan <sornamvairavan@outlook.com>
2022-02-07 10:46:23 -0800

    checking product form issues

--- app/forms/product_form.py
+++ app/forms/product_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, URLField, DecimalField, IntegerField, SelectField
-from wtforms.validators import DataRequired, Length, URL, NumberRange, ValidationError
+from wtforms.validators import DataRequired, Length, NumberRange, ValidationError
 from app.models import Category
 
 categories_list = Category.query.all()
@@ -9,7 +9,7 @@ categories =[(category.name, category.id) for category in categories_list]
 class ProductForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired(), Length(min=1, max=100, message="Title must be less than 100 characters")])
     description = StringField('Description', validators=[DataRequired(), Length(min=1, max=255, message="Description must be less than 255 characters")])
-    imageUrl = URLField('Image', validators=[DataRequired(), Length(min=1, max=255, message="Image URL must be less than 255 characters"), URL(message="Plese enter a valid URL")])
+    imageUrl = URLField('Image', validators=[DataRequired(), Length(min=1, max=255, message="Image URL must be less than 255 characters")])
     price = DecimalField('Price', validators=[DataRequired()], places=2)
     quantity = IntegerField('Quantity', validators=[DataRequired(), NumberRange(min=1, message="Please enter a valid quantity")])
     category = SelectField('Category', choices=categories, validators=[DataRequired()])
