39259594d9de39a79eab4576c0bc06dee71d3201
Nik <nikblvck@icloud.com>
2022-02-07 13:48:07 -0800

    update

--- react-app/src/components/Cart /Cart.js
+++ react-app/src/components/Cart /Cart.js
@@ -2,12 +2,13 @@ import {useSelector, useEffect} from 'react';
 
 function CartPage() {
 
-  const sessionUser = useSelector(state => state.session.user);
-  const cart = useSelector(state => state.cart.cart);
-  
+
+
+
   return (
     <>
     <h1>Cart</h1>
+ 
     </>
   )
 }
