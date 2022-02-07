4d3a13f01914e46e5ae88c4c75158af8b5756bd8
Sornam Vairavan <sornamvairavan@outlook.com>
2022-02-07 10:15:55 -0800

    updated product form

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, session, request
 from flask_login import login_required
 from app.models import Product, db
+from app.forms import ProductForm
 
 product_routes = Blueprint('products', __name__)
 
@@ -20,8 +21,16 @@ def get_products():
     products = Product.query.all()
     return {'products': [product.to_dict() for product in products]}
 
-@product_routes.route("/<int:id>")
+@product_routes.route('/<int:id>')
 def get_product(id):
     product = Product.query.get(id)
     return product.to_dict()
 
+@product_routes.route('/new', methods=['POST'])
+def add_product():
+    form = ProductForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        new_product = Product(
+            title=
+        )
---
+++ app/forms/product_form.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, URLField, DecimalField, IntegerField, SelectField
+from wtforms.validators import DataRequired, Length, URL, NumberRange, ValidationError
+from app.models import Category
+
+categories_list = Category.query.all()
+categories =[(category.name, category.id) for category in categories_list.keys()]
+
+class ProductForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(), Length(min=1, max=100, message="Title must be less than 100 characters")])
+    description = StringField('Description', validators=[DataRequired(), Length(min=1, max=255, message="Description must be less than 255 characters")])
+    imageUrl = URLField('Image', validators=[DataRequired(), Length(min=1, max=255, message="Image URL must be less than 255 characters"), URL(message="Plese enter a valid URL")])
+    price = DecimalField('Price', validators=[DataRequired()], places=2)
+    quantity = IntegerField('Quantity', validators=[DataRequired(), NumberRange(min=1, message="Please enter a valid quantity")])
+    categoryId = SelectField('Category', choices=categories, validators=[DataRequired()])
+
