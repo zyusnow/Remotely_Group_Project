acb784031af5f3438e0538a51858a9bcc6eff372
Sornam Vairavan <sornamvairavan@outlook.com>
2022-02-07 09:29:17 -0800

    updated get products route

---
+++ app/api/product_routes.py
@@ -0,0 +1,22 @@
+from flask import Blueprint, jsonify
+from flask_login import login_required
+from app.models import Product, db
+
+product_routes = Blueprint('products', __name__)
+
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f'{field} : {error}')
+    return errorMessages
+
+
+@product_routes.route('/')
+def get_products():
+    products = Product.query.all()
+    return {'products': [product.to_dict() for product in products]}
+
