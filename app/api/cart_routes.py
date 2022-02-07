52c388b5da13af2e62fecdcc7a7ca403a9d9a3d7
Nik <nikblvck@icloud.com>
2022-02-07 10:44:25 -0800

    updated cart route

--- app/api/cart_routes.py
+++ app/api/cart_routes.py
@@ -5,8 +5,7 @@ from app.models import Cart
 cart_routes = Blueprint('cart', __name__)
 
 @cart_routes.route('/<int:id>')
-@login_required
 def cart(id):
     cart = Cart.query.get(id)
-    jsonify(cart.to_dict())
+    # jsonify(cart.to_dict())
     return '<h1>Cart Route Working, Yay!</h1>'
