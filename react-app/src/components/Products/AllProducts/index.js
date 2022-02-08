ec9599bf7e6dc2e0daa35c25214aa229b4914d1e
Sornam Vairavan <sornamvairavan@outlook.com>
2022-02-07 17:48:41 -0800

    removed small errors and updated find all products link

--- react-app/src/components/Products/AllProducts/index.js
+++ react-app/src/components/Products/AllProducts/index.js
@@ -10,7 +10,6 @@ export default function AllProducts() {
     const dispatch = useDispatch();
     const productsObj = useSelector(state => state?.product?.products)
     const productsArr = Object.values(productsObj)
-    console.log("component/products", productsArr)
 
     useEffect(() => {
         dispatch(getAllProducts())
@@ -23,7 +22,7 @@ export default function AllProducts() {
                 {productsArr?.map(product => (
                     <div className='one_product_container' key={product?.id}>
                         <div className='img_container'>
-                                <img className="img" src={product?.imageUrl} alt={product?.title}></img>
+                                <img className="img" src={product?.imageUrl} alt={product?.category_name}></img>
                         </div>
                         <div className='card_content'>
                             <Link to={`/products/${product.id}`}>{product?.title}</Link>
--- react-app/src/components/Products/NewProduct/index.js
+++ react-app/src/components/Products/NewProduct/index.js
@@ -1,4 +1,7 @@
-
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+import { useHistory, Redirect } from 'react-router-dom'
+import { addOneProduct } from '../../../store/product';
 
 export default function NewProduct() {
     return (
