454edbb82f38144b0f79053468a4430fa1da4e8e
Sornam Vairavan <sornamvairavan@outlook.com>
2022-02-08 13:09:12 -0800

    added routes for post, put and delete reviews on backend

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request, redirect
 from flask_login import login_required, current_user
 from app.models import Review, db
+from app.forms import ReviewForm
 
 review_routes = Blueprint('reviews', __name__)
 
@@ -24,5 +25,45 @@ def get_reviews():
 @review_routes.route('/new', methods=['POST'])
 @login_required
 def add_review():
-    
+    form = ReviewForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        new_review = Review(
+            rating = form.data['rating'],
+            comment = form.data['comment'],
+            userId = int(current_user.id),
+            productId = int(form.data['productId'])
+        )
+        db.session.add(new_review)
+        db.session.commit()
+        return new_review.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
 
+
+@review_routes.route('/<int:id>', methods=['PUT'])
+@login_required
+def edit_review():
+    review = Review.query.get(id)
+
+    form = ReviewForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+        review.rating = form.data['rating'],
+        review.comment = form.data['comment'],
+        review.productId = int(form.data['productId']),
+
+        db.session.commit()
+        return review.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401 
+
+@review_routes.route('/<int:id>', methods=['DELETE'])
+@login_required
+def delete_review():
+    review = Review.query.get(id)
+    if int(current_user.id) == review.userId:
+        db.session.delete(review)
+        db.session.commit()
+        return "Delete successful"
+    else:
+        return 401
--- app/forms/review_form.py
+++ app/forms/review_form.py
@@ -6,3 +6,4 @@ from wtforms.validators import DataRequired, NumberRange
 class ReviewForm(FlaskForm):
     rating = IntegerField('rating', validators=[DataRequired(), NumberRange(min=1, max=5, message='Rating must be between 1 and 5')])
     comment = TextAreaField('comment')
+    productId = IntegerField('productId', validators=[DataRequired()])
