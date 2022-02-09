f365d8a10d1964578c27551d62de6cf858b468bf
Nik <nikblvck@icloud.com>
2022-02-09 14:26:12 -0800

    displaying products in the cart

--- react-app/src/components/Cart /index.js
+++ react-app/src/components/Cart /index.js
@@ -2,11 +2,13 @@ import { useEffect, useState} from 'react';
 import {useDispatch, useSelector } from 'react-redux';
 import { loadCart } from '../../store/cart'
 import {getAllProducts} from '../../store/product'
+import './Cart.css'
 
 function CartPage() {
   const id = useSelector((state) => state.session?.user.id);
   const cartItemsArray = useSelector((state) => state.carts?.cart);
   const products = useSelector((state) => state.product?.products )
+  const user = useSelector((state) => state.session?.user)
   const dispatch = useDispatch();
   const [cart, setCart] = useState({});
   const [isLoaded, setIsLoaded] = useState(false);
@@ -18,14 +20,37 @@ function CartPage() {
     dispatch(loadCart(id));
   }, [dispatch]);
 
-  // const productIds = cartItemsArray?.map(cartItem => {
-  //  return cartItem.productId
-  // })
+  const productIds = cartItemsArray?.map(cartItem => {
+   return cartItem.productId
+  })
+ console.log(productIds)
+  const cartProducts = productIds?.map(productId => {
+    return products[productId]
+  })
+
 
   return (
     <>
-      <h1>Cart</h1>
-      <p>This is just fluff {id}</p>
+      <h1>My Cart</h1>
+
+      <div>
+        <ul className="cartItems">
+          {cartProducts?.map((product) => {
+            if (product) {
+              return (
+                <>
+                  <li key={product.img} className="cartImage">
+                    <img src={product.imageUrl} />
+                  </li>
+                  <li key={product.price} className="cartItemPrice">
+                    {product.price}
+                  </li>
+                </>
+              );
+            }
+          })}
+        </ul>
+      </div>
     </>
   );
 }
