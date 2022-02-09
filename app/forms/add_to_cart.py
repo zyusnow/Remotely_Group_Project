ebd628fa77ca1c8c32fc5f456f92fb797166769c
Nik <nikblvck@icloud.com>
2022-02-09 10:18:38 -0800

    update

--- app/forms/add_to_cart.py
+++ app/forms/add_to_cart.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField
 from wtforms.validators import DataRequired
-from app.models import Product, CartItem, Cart
 
 class AddToCartForm(FlaskForm):
   productId = IntegerField('productId', validators=[DataRequired()])
