3bb7b9f9277f156d1116109508dbae7dd3b77eae
Sornam Vairavan <sornamvairavan@outlook.com>
2022-02-10 15:24:40 -0800

    added user and cart seeders

--- app/seeds/cart.py
+++ app/seeds/cart.py
@@ -2,7 +2,7 @@ from app.models import db, Cart
 
 def seed_cart():
   cart01 = Cart(
-    userId = 1,
+    userId = 1
   )
   cart02 = Cart(
     userId = 2
@@ -10,7 +10,28 @@ def seed_cart():
   cart03 = Cart(
     userId = 3
   )
+  cart04 = Cart(
+    userId = 4
+  )
+  cart05 = Cart(
+    userId = 5
+  )
+  cart06 = Cart(
+    userId = 6
+  )
+  cart07 = Cart(
+    userId = 7
+  )
+  cart08 = Cart(
+    userId = 8
+  )
+  cart09 = Cart(
+    userId = 9
+  )
+  cart10 = Cart(
+    userId = 10
+  )
 
-  db.session.add_all([cart01, cart02, cart03])
+  db.session.add_all([cart01, cart02, cart03, cart04, cart05, cart06, cart07, cart08, cart09, cart10])
   db.session.commit()
 
