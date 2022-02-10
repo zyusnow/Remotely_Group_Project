99f8d22c209fe2c34ffc61e3cc34e1f606833a62
Sornam Vairavan <sornamvairavan@outlook.com>
2022-02-10 11:35:59 -0800

    added some product seeders

--- app/seeds/products.py
+++ app/seeds/products.py
@@ -54,7 +54,7 @@ def seed_products():
   price = 199.99,
   description='Lorem ipsum dolor sit amet, consectetur adispiscing elit, sed do eismod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
   quantity=10,
-  categoryId=3,
+  categoryId=8,
   )
   product07 = Product(
     title = 'L Shaped Office Desk',
@@ -119,7 +119,33 @@ def seed_products():
     quantity=10,
     categoryId=4
   )
-
+  product14 = Product(
+    title = 'Epson® Workforce® Pro WF-4830 Wireless Color Inkjet All-In-One Printer',
+    userId = 3,
+    imageUrl='https://media.officedepot.com/images/f_auto,q_auto,e_sharpen,h_450/products/9940280/9940280_o01_051121/9940280_o01_051121',
+    price = 219.99,
+    description='Lorem ipsum dolor sit amet, consectetur adispiscing elit, sed do eismod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    quantity=10,
+    categoryId=6
+  )
+  product15 = Product(
+    title = 'Realspace® 36"W Steel 5-Shelf Cabinet, Black',
+    userId = 3,
+    imageUrl='https://media.officedepot.com/images/f_auto,q_auto,e_sharpen,h_450/products/945822/945822_o01_120720/945822_o01_120720',
+    price = 359.99,
+    description='Lorem ipsum dolor sit amet, consectetur adispiscing elit, sed do eismod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    quantity=10,
+    categoryId=7
+  )
+  product16 = Product(
+    title = 'LG 32" Class QHD IPS Monitor',
+    userId = 3,
+    imageUrl='https://images.costco-static.com/ImageDelivery/imageService?profileId=12026540&itemId=8325500-847&recipeName=352&viewId=1',
+    price = 219.99,
+    description='Lorem ipsum dolor sit amet, consectetur adispiscing elit, sed do eismod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    quantity=10,
+    categoryId=2
+  )
 
 
   db.session.add(product01)
@@ -135,6 +161,10 @@ def seed_products():
   db.session.add(product11)
   db.session.add(product12)
   db.session.add(product13)
+  db.session.add(product14)
+  db.session.add(product15)
+  db.session.add(product16)
+
   db.session.commit()
 
 def undo_products():
