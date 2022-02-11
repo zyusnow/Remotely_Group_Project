2e739bdf98fceb72821f14b9189b7dd8a071b143
Sornam Vairavan <sornamvairavan@outlook.com>
2022-02-11 11:41:12 -0800

    think search route for heroku is fixed

--- app/api/search_routes.py
+++ app/api/search_routes.py
@@ -3,10 +3,10 @@ from app.models import Product
 
 search_routes = Blueprint('search', __name__)
 
-@search_routes.route('/')
-def search():
-    term = request.args.get('q')
-    searchResult = Product.query.filter(Product.title.ilike(f'%{term}%')).all()
+@search_routes.route('/<q>/')
+def search(q):
+    # term = request.args.get('q')
+    searchResult = Product.query.filter(Product.title.ilike(f'%{q}%')).all()
     if searchResult:
         result = { product.id : product.to_dict() for product in searchResult }
         return { "products" : result }
